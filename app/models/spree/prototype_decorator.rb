8c5c2c222702c377283faf197605941aed2a5db2
Vincent Franco <vince@freshivore.net>
2014-06-12 16:31:55 -0700

    Fixing issue with associations

--- app/models/spree/prototype_decorator.rb
+++ app/models/spree/prototype_decorator.rb
@@ -1,10 +1,10 @@
 Spree::Prototype.class_eval do
-  has_many :properties_prototypes
+  has_many :properties_prototypes, class_name: 'Spree::PropertiesPrototype'
   has_many :properties,
            through: :properties_prototypes,
-           class_name: 'Spree::PropertiesPrototype'
+           class_name: 'Spree::Property'
   has_many :option_types_prototypes
   has_many :option_types,
            through: :option_types_prototypes,
-           class_name: 'Spree::OptionTypesPrototype'
+           class_name: 'Spree::OptionType'
 end
--- spec/models/spree/property_spec.rb
+++ spec/models/spree/property_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 
 describe Spree::Property do
   it { should have_many(:prototypes).through(:properties_prototypes) }
+
+  context 'has_many through' do
+    it 'assigns correctly' do
+      property = FactoryGirl.create :property
+      property.prototypes << FactoryGirl.create(:prototype)
+      expect(property.prototypes.size).to eq 1
+    end
+  end
+
+  context '#find_all_by_prototype' do
+    it 'returns all found properties' do
+      prototype = FactoryGirl.create :prototype
+      expect(Spree::Property.find_all_by_prototype(prototype)).to eq [prototype]
+    end
+  end
 end
