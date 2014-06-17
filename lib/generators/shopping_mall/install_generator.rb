c2b5a4d0c8ffb82586ccdf51433704dcf20afe2f
David Freerksen <dfreerksen@gmail.com>
2014-06-17 14:22:30 -0700

    Changes from Hound

--- lib/generators/shopping_mall/install_generator.rb
+++ lib/generators/shopping_mall/install_generator.rb
@@ -5,8 +5,6 @@ module ShoppingMall
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../../templates', __FILE__)
 
-      class_option :auto_run_migrations, :type => :boolean, :default => false
-
       desc 'Creates initializer and inserts middleware'
 
       def copy_initializer
@@ -31,12 +29,12 @@ module ShoppingMall
       end
 
       def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
-        if run_migrations
-          run 'bundle exec rake db:migrate'
-        else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
-        end
+       res = ask 'Would you like to run the migrations now? [Y/n]'
+       if ['', 'y'].include?(res.downcase)
+         run 'bundle exec rake db:migrate'
+       else
+         puts 'Skipping rake db:migrate, don\'t forget to run it!'
+       end
       end
     end
   end
