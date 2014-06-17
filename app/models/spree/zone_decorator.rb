db91de4fb3f04f559448b9817cb64b8508f8255e
David Freerksen <dfreerksen@gmail.com>
2014-06-16 18:28:24 -0700

    Hound changes

--- app/models/spree/zone_decorator.rb
+++ app/models/spree/zone_decorator.rb
@@ -4,15 +4,15 @@ Spree::Zone.class_eval do
            through: :shipping_methods_zones,
            class_name: 'Spree::ShippingMethod'
 
-  scope :order_by_members, ->{ order(:zone_members_count, :created_at) }
+  scope :order_by_members, -> { order(:zone_members_count, :created_at) }
   scope :where_country_or_state, ->(address) {
     where(Spree::ZoneMember.arel_where_country_or_state(address))
   }
   scope :matching_zones_for, ->(address) {
-    includes(:zone_members)
-      .where_country_or_state(address)
-        .order_by_members
-          .references(:zones)
+    includes(:zone_members).
+      where_country_or_state(address).
+        order_by_members.
+          references(:zones)
   }
 
   class << self
